4af40228004e084afb08f593e91dcc4f3e862eb8
THZoria <50277488+THZoria@users.noreply.github.com>
2021-05-18 16:06:35 +0200

    fix bug buildscriptt
    Fixed a problem with the const inputAppName which did not point correctly to the index

--- build/buildscript.js
+++ build/buildscript.js
@@ -1,12 +1,11 @@
 const { compile } = require('nexe')
-const inputAppName = './index.js'
+const inputAppName = '././index.js'
 const outputAppName = 'NXPayload-Converter'
 
 compile({
   input: inputAppName,
   output: outputAppName,
   build: true,
-  ico: './icon.ico'
 }).then((err) => {
   if (err) throw err
   console.log('success')
