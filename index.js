6ee8d3899325712b6a22b3156a62c6d1a747d97a
Murasaki <matisu64@gmail.com>
2021-05-18 12:41:36 +0200

    Version 1.0.0
    - Add .gitignore (Node)
    - Add index.js
    - Add package.json

---
+++ index.js
@@ -0,0 +1,64 @@
+const { readdirSync, writeFileSync, readFileSync, existsSync, mkdirSync } = require('fs');
+const { createHash } = require('crypto');
+
+async function createBootDat(payload) {
+    let header = Buffer.allocUnsafe(0xE0);
+    header.write('\x43\x54\x43\x61\x65\x72\x20\x42\x4F\x4F\x54\x00', 0);
+    header.write('\x56\x32\x2E\x35', 0xC);
+    header.set(createHash('sha256').update(payload).digest(), 0x10);
+    header.writeUInt32LE(0x40010000, 0x30);
+    header.writeUInt32LE(payload.length, 0x34);
+    header.writeUInt32LE(0, 0x38);
+    header.set(Buffer.alloc(0xA4), 0x3C);
+    let boot = Buffer.allocUnsafe(0x100 + payload.length);
+    boot.set(header, 0);
+    boot.set(createHash('sha256').update(header).digest(), 0xE0);
+    boot.set(payload, 0x100);
+    return boot;
+}; // Original CTCaer script (https://github.com/CTCaer) converted to JavaScript by Pharuxtan (https://github.com/Pharuxtan)
+
+const colors = {
+    'default': (text) => { return `\x1b[38;2;44;52;121m${text}\x1b[39m` },
+    'success': (text) => { return `\x1b[38;2;7;106;0m${text}\x1b[39m` },
+    'warning': (text) => { return `\x1b[38;2;255;116;0m${text}\x1b[39m` },
+    'error'  : (text) => { return `\x1b[38;2;118;1;1m${text}\x1b[39m` }
+};
+
+(async () => {
+    try {
+        console.clear();
+        console.log(colors.default(`     
+  _   _ __   ________           _                 _   _____                           _            
+ | \\ | |\\ \\ / /| ___ \\         | |               | | /  __ \\                         | |           
+ |  \\| | \\ V / | |_/ /_ _ _   _| | ___   __ _  __| | | /  \\/ ___  _ ____   _____ _ __| |_ ___ _ __ 
+ | . \` | /   \\ |  __/ _\` | | | | |/ _ \\ / _\` |/ _\` | | |    / _ \\| '_ \\ \\ / / _ \\ '__| __/ _ \\ '__|
+ | |\\  |/ /^\\ \\| | | (_| | |_| | | (_) | (_| | (_| | | \\__/\\ (_) | | | \\ V /  __/ |  | ||  __/ |   
+ \\_| \\_/\\/   \\/\\_|  \\__,_|\\__, |_|\\___/ \\__,_|\\__,_|  \\____/\\___/|_| |_|\\_/ \\___|_|   \\__\\___|_|   
+                           __/ |                                                                   
+                          |___/                                             v1.0.0 By MurasakiNX                                                            
+        `));
+
+        const bins = readdirSync('./').filter(f => f.endsWith('.bin'));
+
+        if (bins.length == 0) {
+            return console.log(colors.warning('[WARNING] - Please make sure that your Nintendo Switch payload files (.bin) are in the same folder as the executable'));
+        };
+
+        if (!existsSync('./generated')) {
+            mkdirSync('./generated');
+            console.log(colors.default('\n[Info] - The generated folder has just been created'));
+        };
+
+        let i = 0;
+
+        for (bin of bins) {
+            console.log(colors.default(`[${++i}/${bins.length}] - Creating boot.dat file from ${bin}`));
+            writeFileSync(`./generated/${bin}_boot.dat`, await createBootDat(readFileSync(bin)));
+            console.log(colors.success(`[BOOT.DAT] - ${bin}_boot.dat has just been generated!\n`));
+        };
+
+        console.log(colors.success('[SUCCESS] - All Nintendo Switch payload files (.bin) have been converted to boot.dat in the generated folder.'));
+    } catch (e) {
+        console.log(colors.error(`[ERROR] - An error has occured: ${e.stack}.`));
+    };
+})();
