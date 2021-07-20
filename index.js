9f4f04db4e3e4e665695e70f830194e2844a1f09
Mura⚡ <matisu64@gmail.com>
2021-07-21 00:48:13 +0200

    1.3.1

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const { readdirSync, writeFileSync, readFileSync, existsSync, mkdirSync, statSync, unlinkSync } = require('fs');
 const { createHash } = require('crypto');
-const process = require('process');
+const { existsSync, mkdirSync, readdirSync, readFileSync, statSync, unlinkSync, writeFileSync } = require('fs');
 const fetch = require('node-fetch');
 
-async function createBootDat(payload) {
-    let header = Buffer.allocUnsafe(0xE0);
-    header.write('\x43\x54\x43\x61\x65\x72\x20\x42\x4F\x4F\x54\x00', 0);
-    header.write('\x56\x32\x2E\x35', 0xC);
-    header.set(createHash('sha256').update(payload).digest(), 0x10);
-    header.writeUInt32LE(0x40010000, 0x30);
-    header.writeUInt32LE(payload.length, 0x34);
-    header.writeUInt32LE(0, 0x38);
-    header.set(Buffer.alloc(0xA4), 0x3C);
-    let boot = Buffer.allocUnsafe(0x100 + payload.length);
-    boot.set(header, 0);
-    boot.set(createHash('sha256').update(header).digest(), 0xE0);
-    boot.set(payload, 0x100);
-    return boot;
-}; // Original CTCaer script (https://github.com/CTCaer) converted to JavaScript by Pharuxtan (https://github.com/Pharuxtan)
+const LINKS_FILE = `${process.cwd()}/links.txt`;
+const GENERATED_FOLDER = `${process.cwd()}/generated`;
 
 const colors = {
     'default': (text) => { return `\x1b[38;2;44;52;121m${text}\x1b[39m` },
@@ -26,119 +12,88 @@ const colors = {
     'error'  : (text) => { return `\x1b[38;2;118;1;1m${text}\x1b[39m` }
 };
 
-function exit() {
-    console.log(colors.default('\n[EXIT] - Press any key to exit'));
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
-    process.stdin.on('data', process.exit.bind(process, 0));
+const createBootDat = async (payload) => {
+    const header = Buffer.allocUnsafe(0xE0);
+    header.write('\x43\x54\x43\x61\x65\x72\x20\x42\x4F\x4F\x54\x00', 0);
+    header.write('\x56\x32\x2E\x35', 0xC);
+    header.set(createHash('sha256').update(payload).digest(), 0x10);
+    header.writeUInt32LE(0x40010000, 0x30);
+    header.writeUInt32LE(payload.length, 0x34);
+    header.writeUInt32LE(0, 0x38);
+    header.set(Buffer.alloc(0xA4), 0x3C);
+    
+    const boot = Buffer.allocUnsafe(0x100 + payload.length);
+    boot.set(header, 0);
+    boot.set(createHash('sha256').update(header).digest(), 0xE0);
+    boot.set(payload, 0x100);
+    return boot;
 };
 
 (async () => {
-    try {
-        console.clear();
-        console.log(colors.default(`     
-  _   _ __   ________           _                 _   _____                           _            
- | \\ | |\\ \\ / /| ___ \\         | |               | | /  __ \\                         | |           
- |  \\| | \\ V / | |_/ /_ _ _   _| | ___   __ _  __| | | /  \\/ ___  _ ____   _____ _ __| |_ ___ _ __ 
- | . \` | /   \\ |  __/ _\` | | | | |/ _ \\ / _\` |/ _\` | | |    / _ \\| '_ \\ \\ / / _ \\ '__| __/ _ \\ '__|
- | |\\  |/ /^\\ \\| | | (_| | |_| | | (_) | (_| | (_| | | \\__/\\ (_) | | | \\ V /  __/ |  | ||  __/ |   
- \\_| \\_/\\/   \\/\\_|  \\__,_|\\__, |_|\\___/ \\__,_|\\__,_|  \\____/\\___/|_| |_|\\_/ \\___|_|   \\__\\___|_|   
-                           __/ |                                                                   
-                          |___/                                     v1.3.0 By MurasakiNX & Zoria                                                          
-        `));
+    console.clear();
+    console.log(colors.default(`     
+_   _ __   ________           _                 _   _____                           _            
+| \\ | |\\ \\ / /| ___ \\         | |               | | /  __ \\                         | |           
+|  \\| | \\ V / | |_/ /_ _ _   _| | ___   __ _  __| | | /  \\/ ___  _ ____   _____ _ __| |_ ___ _ __ 
+| . \` | /   \\ |  __/ _\` | | | | |/ _ \\ / _\` |/ _\` | | |    / _ \\| '_ \\ \\ / / _ \\ '__| __/ _ \\ '__|
+| |\\  |/ /^\\ \\| | | (_| | |_| | | (_) | (_| | (_| | | \\__/\\ (_) | | | \\ V /  __/ |  | ||  __/ |   
+\\_| \\_/\\/   \\/\\_|  \\__,_|\\__, |_|\\___/ \\__,_|\\__,_|  \\____/\\___/|_| |_|\\_/ \\___|_|   \\__\\___|_|   
+                       __/ |                                                                   
+                      |___/                                     v1.3.1 By MurasakiNX & Zoria                                                          
+    `));
 
-        if (!existsSync('generated')) {
-            mkdirSync('generated');
-            console.log(colors.default('[GENERATED FOLDER] - The generated folder has just been created\n'));
-        } else if (existsSync('generated') && statSync('./generated').isFile()) {
-            unlinkSync('generated');
-            mkdirSync('generated');
-            console.log(colors.default('[GENERATED FOLDER] - The generated folder has just been created\n'));
-        };
-
-        let i = 0;
-
-        if (existsSync('links.txt') && statSync('links.txt').isFile()) {
-            let links = readFileSync('links.txt', 'utf8').trim().split(',');
-            if (links.join('') == '')
-                console.log(colors.warning('[NO LINKS] - links.txt is an empty file\n'));
-            else {
-                links = Array.from(new Set(links)); // Removes duplicate links
-
-                for (let link of links) {
-                    try {
-                        link = new URL(link).href;
-                        let file = await fetch(link).then(res => res);
-
-                        if (file.headers.get('content-disposition')) {
-                            let fileName = file.headers.get('content-disposition').split('filename=')[1].split(';')[0];
-                            if (!fileName.endsWith('.bin')) {
-                                console.log(colors.error(`[NOT BIN] - ${fileName} is not a .bin file\n`));
-                                continue;
-                            } else if (file.headers.get('content-length') == 0) {
-                                console.log(colors.error(`[EMPTY FILE] - ${fileName} is an empty file\n`));
-                                continue;
-                            } else if (file.headers.get('content-length') > 256000) {
-                                console.log(colors.error(`[TOO BIG FILE] - ${fileName} is a file larger than 256kB\n`));
-                                continue;
-                            };
-
-                            console.log(colors.default(`[${++i}/${links.length}] - Creating boot.dat file from ${fileName}`));
-                            if (!existsSync(`generated/online_${fileName}`)) mkdirSync(`generated/online_${fileName}`);
-                            writeFileSync(`generated/online_${fileName}/boot.dat`, await createBootDat(await file.buffer()));
-                            console.log(colors.success(`[BOOT.DAT ONLINE] - ${fileName} has just been generated\n`));
-                        } else
-                            console.log(colors.error(`[UNVALID LINK] - ${link} is not a file link\n`));
-                    } catch (e) {
-                        if (e.input || e.code == 'ENOTFOUND') {
-                            const [githubFile, version = 'latest'] = link.trim().toLowerCase().split('@');
-                            const file = await fetch(`https://github.com/MurasakiNX/NXPayload-Converter/blob/main/binaries/${githubFile}/${githubFile}@${version}.bin?raw=true`);
-                            
-                            if (file.ok) {
-                                console.log(colors.default(`[${++i}/${links.length}] - Creating boot.dat file from ${githubFile}@${version}`));
-                                if (!existsSync(`generated/github_${githubFile}@${version}`)) mkdirSync(`generated/github_${githubFile}@${version}`);
-                                writeFileSync(`generated/github_${githubFile}@${version}/boot.dat`, await createBootDat(await file.buffer()));
-                                console.log(colors.success(`[BOOT.DAT GITHUB BINARIES] - ${githubFile}@${version} has just been generated\n`));
-                            } else
-                                console.log(colors.error(`[UNVALID LINK] - ${link} is not a valid link or you are not connected to the Internet\n`));
-                        } else {
-                            console.log(colors.error(`[ONLINE ERROR] - An error has occured: ${e.stack}\n`));
-                            break;
-                        };
-                    };
-                };
-                console.log(colors.success('[ONLINE DONE] - All Nintendo Switch payload files that did not display an error (.bin) have been converted to boot.dat in the generated folder\n'));
-            };
-        };
-
-        i = 0;
-
-        const bins = readdirSync('./').filter(f => f.endsWith('.bin'));
-
-        if (bins.length == 0) {
-            console.log(colors.warning('[NO BINS] - Please make sure that your Nintendo Switch payload files (.bin) are in the same folder as the executable'));
-            return exit();
-        };
-
-        for (bin of bins) {
-            let fileSize = statSync(bin).size;
-
-            if (fileSize == 0) {
-                console.log(colors.error(`[EMPTY FILE] - ${bin} is an empty file\n`));
-                continue;
-            } else if (fileSize > 256000) {
-                console.log(colors.error(`[TOO BIG FILE] - ${bin} is a file larger than 256kB\n`));
-                continue;
-            };
-
-            console.log(colors.default(`[${++i}/${bins.length}] - Creating boot.dat file from ${bin}`));
-            if (!existsSync(`generated/local_${bin}`)) mkdirSync(`generated/local_${bin}`);
-            writeFileSync(`generated/local_${bin}/boot.dat`, await createBootDat(readFileSync(bin)));
-            console.log(colors.success(`[BOOT.DAT LOCAL] - ${bin} has just been generated\n`));
-        };
-        console.log(colors.success('[LOCAL DONE] - All Nintendo Switch payload files that did not display an error (.bin) have been converted to boot.dat in the generated folder'));
-    } catch (e) {
-        console.log(colors.error(`[LOCAL ERROR] - An error has occured: ${e.stack}`));
+    if (!existsSync(GENERATED_FOLDER))
+        mkdirSync(GENERATED_FOLDER);
+    else if (!statSync(GENERATED_FOLDER).isDirectory()) {
+        unlinkSync(GENERATED_FOLDER);
+        mkdirSync(GENERATED_FOLDER);
     };
-    exit();
-})();
+
+    const links = Array.from(new Set([...readdirSync(process.cwd()).filter(f => f.endsWith('.bin') && statSync(f).isFile()), ...existsSync(LINKS_FILE) && statSync(LINKS_FILE).isFile() ? readFileSync('links.txt', 'utf8').trim().split(',') : []])).filter(link => link !== '');
+
+    for (let link of links) {
+        const index = links.indexOf(link) + 1;
+        if (link.endsWith('.bin') && existsSync(`${process.cwd()}/${link}`) && statSync(`${process.cwd()}/${link}`).isFile()) {
+            const fileSize = statSync(link).size;
+            if (fileSize == 0 || fileSize > 256000)
+                console.log(colors.error(`[FILE ERROR] - ${link}'s size must be between 0B and 256kB.\n`));
+            else {
+                console.log(colors.default(`[${index}/${links.length}] - Generating boot.dat file from ${link}...`));
+                if (!existsSync(`${GENERATED_FOLDER}/file_${link}`)) 
+                    mkdirSync(`${GENERATED_FOLDER}/file_${link}`);
+                writeFileSync(`${GENERATED_FOLDER}/file_${link}/boot.dat`, await createBootDat(readFileSync(link)));
+                console.log(colors.success(`[BOOT.DAT FILE] - ${link} has just been generated !\n`));
+            };
+        } else {
+            const [githubFile, version = 'latest'] = link.trim().toLowerCase().split('@');
+            let isCorrectLink = false;
+            try {
+                link = new URL(link).href;
+                isCorrectLink = true;
+            } catch (e) {
+                link = `https://github.com/MurasakiNX/NXPayload-Converter/blob/main/binaries/${githubFile}/${githubFile}@${version}.bin?raw=true`;
+            };
+
+            const file = await fetch(link);
+            if (!file.ok)
+                console.log(colors.warning(`[ONLINE ERROR] - ${link} is not a good link.\n`));
+            else {
+                const fileName = isCorrectLink ? file.headers.get('content-disposition')?.split('filename=')[1].split(';')[0] || link.split('/').pop() : `${githubFile}@${version}.bin`;
+                if (!fileName.endsWith('.bin') || file.headers.get('content-length') == 0 || file.headers.get('content-length') > 256000) 
+                    console.log(colors.error(`[FILE ERROR] - ${fileName}'s size must be between 0B and 256kB and the file type must be .bin.\n`));
+                else {
+                    console.log(colors.default(`[${index}/${links.length}] - Generating boot.dat file from ${fileName}...`));
+                    if (!existsSync(`${GENERATED_FOLDER}/online_${fileName}`)) 
+                        mkdirSync(`${GENERATED_FOLDER}/online_${fileName}`);
+                    writeFileSync(`${GENERATED_FOLDER}/online_${fileName}/boot.dat`, await createBootDat(await file.buffer()));
+                    console.log(colors.success(`[BOOT.DAT ONLINE] - ${fileName} has just been generated !\n`));
+                };
+            };
+        };
+    };
+
+    console.log(colors.success('[SUCCESS] - The boot.dat files have just been correctly generated !'), colors.warning('\n[EXIT] - Press any key to exit.'));
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));
+})().catch(({stack}) => console.log(colors.error(`An error has occured: ${stack}`)));
